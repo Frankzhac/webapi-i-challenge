dc3f4654ba3deed6880d4c8ea154ab43f7e26443
Frankzhac <Frank.hemle@gmail.com>
2019-06-03 18:08:12 -0400

    save new user in the db

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ server.get('/', (req, res) => {
   res.send('Hello Word');
 });
 
+// Endpoint starts below
+
+server.post('/api/users', (req, res) => {
+  const body = req.body;
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({
+      errorMessage: "Please provide name and bio for the user."
+    });
+  }
+  // add/save new user in the db
+  db.add(body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        error: "The users information could not be retrieved."
+      })
+    })
+})
 
 server.listen(port, () => {
   console.log(`Server is Running on http://localhost:${port}`)
