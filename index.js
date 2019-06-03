e4a1589426923108852a7f6c54b7f396a49ade59
Frankzhac <Frank.hemle@gmail.com>
2019-06-03 17:42:58 -0400

    Server setup and listening

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db')
+
+const server = express();
+server.use(express.json());
+
+const port = 4000;
+
+server.get('/', (req, res) => {
+  res.send('Hello Word');
+});
+
+
+server.listen(port, () => {
+  console.log(`Server is Running on http://localhost:${port}`)
+});
